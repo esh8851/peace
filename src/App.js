a7590bed151fcb13ad53532cd5e7be50dea30834
esh8851 <esh8851@gmail.com>
2024-11-08 09:25:14 +0900

    20241108

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React716
         </a>
       </header>
     </div>
